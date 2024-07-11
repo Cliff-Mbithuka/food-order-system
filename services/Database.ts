0ac9b16f9cff1be7dead81ac5ed6fd4a1eaeaf95
Clifford Musyoka <your_email@example.com>
2024-07-11 21:23:02 +0300

    Connect db

---
+++ services/Database.ts
@@ -0,0 +1,17 @@
+
+import mongoose from 'mongoose';
+import { MONGO_URI } from '../config';
+
+export default async () => {
+    try {
+        mongoose.connect(MONGO_URI,{
+            // useUnifiedTopology: true,
+            // useCreateIndex: true
+        })
+            console.log('MongoDB Connected...')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
