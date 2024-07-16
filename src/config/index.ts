a3fb30a1dd387a350ee3a2b47beed0cf4e48574f
Clifford Musyoka <your_email@example.com>
2024-07-16 23:17:58 +0300

    connect to Db and Server

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,6 @@
-export const MONGO_URI = 'mongodb://127.0.0.1:27017/food';
+ export const MONGO_URI = 'mongodb+srv://Cliff:2673@cluster0.ndjrymi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// 'mongodb://127.0.0.1:27017/food';
 
 export const APP_SECRET = "Our_App_Secret";
+
+export const PORT = process.env.PORT || 2485
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import App from './services/ExpressApp'
 import dbConnection from './services/Database';
+import { PORT } from "./config";
 
 
 const StartServer = async () => {
@@ -11,8 +12,8 @@ const StartServer = async () => {
 
     await App(app);
 
-    app.listen(2485, () => {
-        console.log("Listening to port 2485");
+    app.listen(PORT, () => {
+        console.log(`Listening to port ${PORT}`);
         
     })
 }
