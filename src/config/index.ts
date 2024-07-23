1d0e1e7182bbf839f7d4030739faddf37b96f8ff
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-07-23 22:04:02 +0300

    modify

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
- export const MONGO_URI = 'mongodb+srv://Cliff:2673@cluster0.ndjrymi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  export const MONGO_URI = 'mongodb+srv://Cliff:2673@cluster0.ndjrymi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 // 'mongodb://127.0.0.1:27017/food';
 
 export const APP_SECRET = "Our_App_Secret";
 
-export const PORT = process.env.PORT || 2485
+export const PORT = process.env.PORT || 2485;
