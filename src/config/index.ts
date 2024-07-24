bdc605dcc265741c21b8344cd4a8879aa1372bdf
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-07-24 22:24:58 +0300

    Modify db

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
-  export const MONGO_URI = 'mongodb+srv://Cliff:2673@cluster0.ndjrymi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-// 'mongodb://127.0.0.1:27017/food';
+  export const MONGO_URI = 'mongodb://127.0.0.1:27017/food';
+
+//'mongodb+srv://Cliff:2673@cluster0.ndjrymi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 export const APP_SECRET = "Our_App_Secret";
 
--- src/services/Database.ts
+++ src/services/Database.ts
@@ -5,8 +5,8 @@ import { MONGO_URI } from '../config';
 export default async () => {
     try {
         mongoose.connect(MONGO_URI,{
-            // useUnifiedTopology: true,
-            // useCreateIndex: true
+           // useUnifiedTopology: true,
+           // useCreateIndex: true
         })
             console.log('MongoDB Connected...')
     } catch (ex) {
